763410150331be4f7b551d643e0a6c5cef44d6c2
zhouZH704403055 <704403055hao@sina.com>
2018-10-05 22:54:09 +0800

    1到 100 的所有整数中出现多少次数字9

---
+++ 次数/test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int i = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 10 == 9)
+		{
+			count++;
+		}
+		if (i / 10 == 9)
+		{
+			count++;
+		}
+	}
+	printf("%d\n", count);
+	return 0;
+}
