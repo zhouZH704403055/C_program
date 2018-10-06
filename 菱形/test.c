ff286be9bcb7bc6838b64fcf5a1a3cef8d1465de
zhouZH704403055 <704403055hao@sina.com>
2018-10-06 18:40:26 +0800

    输入行数n，输出相应行数为2n-1的菱形

---
+++ 菱形/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	scanf("%d", &line);
+	for (int i = 0; i < line; i++)
+	{
+		int j = 0;
+		for (j = 0; j < line - i - 1; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < line - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < i + 1; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j < 2 * (line - i - 1) - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
